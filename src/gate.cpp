abd1c5af824d224a1b2b6fce2e7cfe3a3ff59c79
Keichi Takahashi <hello@keichi.dev>
2023-08-15 20:45:22 +0900

    Add X, Y, Z, CZ gates with gate-specific optimizations

--- src/gate.cpp
+++ src/gate.cpp
@@ -232,6 +232,85 @@ void apply_two_qubit_gate(std::vector<double> &state_re, std::vector<double> &st
     }
 }
 
+void apply_x_gate_opt(std::vector<double> &state_re, std::vector<double> &state_im, UINT BATCH_SIZE,
+                      UINT n, UINT target)
+{
+    ITYPE mask = 1ULL << target;
+    ITYPE lo_mask = mask - 1;
+    ITYPE hi_mask = ~lo_mask;
+
+#pragma omp parallel for
+    for (ITYPE i = 0; i < 1ULL << (n - 1); i++) {
+        ITYPE i0 = ((i & hi_mask) << 1) | (i & lo_mask);
+        ITYPE i1 = i0 | mask;
+
+#pragma omp simd
+        for (int sample = 0; sample < BATCH_SIZE; sample++) {
+            double tmp0_re = state_re[sample + i0 * BATCH_SIZE];
+            double tmp0_im = state_im[sample + i0 * BATCH_SIZE];
+            double tmp1_re = state_re[sample + i1 * BATCH_SIZE];
+            double tmp1_im = state_im[sample + i1 * BATCH_SIZE];
+
+            state_re[sample + i0 * BATCH_SIZE] = tmp1_re;
+            state_im[sample + i0 * BATCH_SIZE] = tmp1_im;
+
+            state_re[sample + i1 * BATCH_SIZE] = tmp0_re;
+            state_im[sample + i1 * BATCH_SIZE] = tmp0_im;
+        }
+    }
+}
+
+void apply_y_gate_opt(std::vector<double> &state_re, std::vector<double> &state_im, UINT BATCH_SIZE,
+                      UINT n, UINT target)
+{
+    ITYPE mask = 1ULL << target;
+    ITYPE lo_mask = mask - 1;
+    ITYPE hi_mask = ~lo_mask;
+
+#pragma omp parallel for
+    for (ITYPE i = 0; i < 1ULL << (n - 1); i++) {
+        ITYPE i0 = ((i & hi_mask) << 1) | (i & lo_mask);
+        ITYPE i1 = i0 | mask;
+
+#pragma omp simd
+        for (int sample = 0; sample < BATCH_SIZE; sample++) {
+            double tmp0_re = state_re[sample + i0 * BATCH_SIZE];
+            double tmp0_im = state_im[sample + i0 * BATCH_SIZE];
+            double tmp1_re = state_re[sample + i1 * BATCH_SIZE];
+            double tmp1_im = state_im[sample + i1 * BATCH_SIZE];
+
+            state_re[sample + i0 * BATCH_SIZE] = tmp1_im;
+            state_im[sample + i0 * BATCH_SIZE] = -tmp1_re;
+
+            state_re[sample + i1 * BATCH_SIZE] = tmp0_im;
+            state_im[sample + i1 * BATCH_SIZE] = -tmp0_re;
+        }
+    }
+}
+
+void apply_z_gate_opt(std::vector<double> &state_re, std::vector<double> &state_im, UINT BATCH_SIZE,
+                      UINT n, UINT target)
+{
+    ITYPE mask = 1ULL << target;
+    ITYPE lo_mask = mask - 1;
+    ITYPE hi_mask = ~lo_mask;
+
+#pragma omp parallel for
+    for (ITYPE i = 0; i < 1ULL << (n - 1); i++) {
+        ITYPE i0 = ((i & hi_mask) << 1) | (i & lo_mask);
+        ITYPE i1 = i0 | mask;
+
+#pragma omp simd
+        for (int sample = 0; sample < BATCH_SIZE; sample++) {
+            double tmp1_re = state_re[sample + i1 * BATCH_SIZE];
+            double tmp1_im = state_im[sample + i1 * BATCH_SIZE];
+
+            state_re[sample + i1 * BATCH_SIZE] = -tmp1_re;
+            state_im[sample + i1 * BATCH_SIZE] = -tmp1_im;
+        }
+    }
+}
+
 void apply_h_gate(std::vector<double> &state_re, std::vector<double> &state_im, UINT BATCH_SIZE,
                   UINT n, UINT target)
 {
@@ -309,6 +388,43 @@ void apply_cnot_gate(std::vector<double> &state_re, std::vector<double> &state_i
     apply_two_qubit_gate(state_re, state_im, BATCH_SIZE, n, matrix_re, matrix_im, target, control);
 }
 
+void apply_cnot_gate_opt(std::vector<double> &state_re, std::vector<double> &state_im,
+                         UINT BATCH_SIZE, UINT n, UINT target, UINT control)
+{
+    ITYPE target_mask = 1ULL << target;
+    ITYPE control_mask = 1ULL << control;
+
+    UINT min_qubit_index = std::min(target, control);
+    UINT max_qubit_index = std::max(target, control);
+    ITYPE min_qubit_mask = 1ULL << min_qubit_index;
+    ITYPE max_qubit_mask = 1ULL << (max_qubit_index - 1);
+    ITYPE lo_mask = min_qubit_mask - 1;
+    ITYPE mid_mask = (max_qubit_mask - 1) ^ lo_mask;
+    ITYPE hi_mask = ~(max_qubit_mask - 1);
+
+#pragma omp parallel for
+    for (ITYPE i = 0; i < 1ULL << (n - 2); i++) {
+        ITYPE i00 = ((i & hi_mask) << 2) | ((i & mid_mask) << 1) | ((i & lo_mask));
+        ITYPE i01 = i00 | target_mask;
+        ITYPE i10 = i00 | control_mask;
+        ITYPE i11 = i00 | control_mask | target_mask;
+
+#pragma omp simd
+        for (int sample = 0; sample < BATCH_SIZE; sample++) {
+            double tmp10_re = state_re[sample + i10 * BATCH_SIZE];
+            double tmp10_im = state_im[sample + i10 * BATCH_SIZE];
+            double tmp11_re = state_re[sample + i11 * BATCH_SIZE];
+            double tmp11_im = state_im[sample + i11 * BATCH_SIZE];
+
+            state_re[sample + i10 * BATCH_SIZE] = tmp11_re;
+            state_im[sample + i10 * BATCH_SIZE] = tmp11_im;
+
+            state_re[sample + i11 * BATCH_SIZE] = tmp10_re;
+            state_im[sample + i11 * BATCH_SIZE] = tmp10_im;
+        }
+    }
+}
+
 void apply_cz_gate(std::vector<double> &state_re, std::vector<double> &state_im, UINT BATCH_SIZE,
                    UINT n, UINT target, UINT control)
 {
@@ -316,4 +432,36 @@ void apply_cz_gate(std::vector<double> &state_re, std::vector<double> &state_im,
     static double matrix_im[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 
     apply_two_qubit_gate(state_re, state_im, BATCH_SIZE, n, matrix_re, matrix_im, target, control);
+}
+
+void apply_cz_gate_opt(std::vector<double> &state_re, std::vector<double> &state_im,
+                       UINT BATCH_SIZE, UINT n, UINT target, UINT control)
+{
+    ITYPE target_mask = 1ULL << target;
+    ITYPE control_mask = 1ULL << control;
+
+    UINT min_qubit_index = std::min(target, control);
+    UINT max_qubit_index = std::max(target, control);
+    ITYPE min_qubit_mask = 1ULL << min_qubit_index;
+    ITYPE max_qubit_mask = 1ULL << (max_qubit_index - 1);
+    ITYPE lo_mask = min_qubit_mask - 1;
+    ITYPE mid_mask = (max_qubit_mask - 1) ^ lo_mask;
+    ITYPE hi_mask = ~(max_qubit_mask - 1);
+
+#pragma omp parallel for
+    for (ITYPE i = 0; i < 1ULL << (n - 2); i++) {
+        ITYPE i00 = ((i & hi_mask) << 2) | ((i & mid_mask) << 1) | ((i & lo_mask));
+        ITYPE i01 = i00 | target_mask;
+        ITYPE i10 = i00 | control_mask;
+        ITYPE i11 = i00 | control_mask | target_mask;
+
+#pragma omp simd
+        for (int sample = 0; sample < BATCH_SIZE; sample++) {
+            double tmp11_re = state_re[sample + i11 * BATCH_SIZE];
+            double tmp11_im = state_im[sample + i11 * BATCH_SIZE];
+
+            state_re[sample + i11 * BATCH_SIZE] = -tmp11_re;
+            state_im[sample + i11 * BATCH_SIZE] = -tmp11_im;
+        }
+    }
 }
