054e0aacf3cd4cbbce36f8f36760fcfc7f2a6b13
Keichi Takahashi <hello@keichi.dev>
2023-09-06 16:41:15 +0900

    Refactor benchmark apps

--- src/main.cpp
+++ src/main.cpp
@@ -9,54 +9,70 @@
 
 int main(int argc, char *argv[])
 {
-    cxxopts::Options options("batched-qsim-ve", "Batched quantum circuit simulator for VE");
+    cxxopts::Options options("qsim-gate-bench", "Basic gate benchmark");
     // clang-format off
     options.add_options()
         ("samples", "# of samples", cxxopts::value<int>()->default_value("1000"))
-        ("target", "Target qubit (round-robin if -1)", cxxopts::value<int>()->default_value("-1"))
+        ("target", "Target qubit", cxxopts::value<int>()->default_value("0"))
+        ("control", "Control qubit", cxxopts::value<int>()->default_value("1"))
         ("depth", "Depth of the circuit", cxxopts::value<int>()->default_value("10"))
         ("qubits", "# of qubits", cxxopts::value<int>()->default_value("10"))
         ("batch-size", "Batch size", cxxopts::value<int>()->default_value("1000"))
-        ("trials", "# of trials", cxxopts::value<int>()->default_value("10"));
+        ("trials", "# of trials", cxxopts::value<int>()->default_value("10"))
+        ("gate", "Gate to act", cxxopts::value<std::string>()->default_value("RX"))
+        ("help", "Print usage");
     // clang-format on
 
     auto result = options.parse(argc, argv);
 
-    const int N_SAMPLES = result["samples"].as<int>();
-    const int TARGET = result["target"].as<int>();
-    const int DEPTH = result["depth"].as<int>();
-    const int N_QUBITS = result["qubits"].as<int>();
-    const int BATCH_SIZE = result["batch-size"].as<int>();
-    const int N_TRIALS = result["trials"].as<int>();
+    int n_samples = result["samples"].as<int>();
+    int target = result["target"].as<int>();
+    int control = result["control"].as<int>();
+    int depth = result["depth"].as<int>();
+    int n_qubits = result["qubits"].as<int>();
+    int batch_size = result["batch-size"].as<int>();
+    int n_trials = result["trials"].as<int>();
+    std::string gate_name = result["gate"].as<std::string>();
+
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
 
     std::random_device seed_gen;
     std::mt19937 engine(seed_gen());
-    std::uniform_real_distribution<double> dist(0.0, M_PI);
+    std::uniform_real_distribution<double> dist(0.0, M_PI * 2);
     std::vector<double> durations;
-    State state(N_QUBITS, BATCH_SIZE);
+    State state(n_qubits, batch_size);
 
     // Warmup run
-    for (int batch = 0; batch < N_SAMPLES; batch += BATCH_SIZE) {
-        for (int d = 0; d < DEPTH; d++) {
-            for (int i = 0; i < N_QUBITS; i++) {
-                state.act_rx_gate(dist(engine), TARGET);
-            }
-            for (int i = 0; i < N_QUBITS; i++) {
-                state.act_cnot_gate(TARGET, (TARGET + 1) % N_QUBITS);
+    for (int batch = 0; batch < n_samples; batch += batch_size) {
+        for (int d = 0; d < depth; d++) {
+            if (gate_name == "RX") {
+                state.act_rx_gate(dist(engine), target);
+            } else if (gate_name == "H") {
+                state.act_h_gate(target);
+            } else if (gate_name == "T") {
+                state.act_t_gate(target);
+            } else if (gate_name == "CNOT") {
+                state.act_cnot_gate(target, control);
             }
         }
     }
 
-    for (int trial = 0; trial < N_TRIALS; trial++) {
+    for (int trial = 0; trial < n_trials; trial++) {
         auto start_time = std::chrono::high_resolution_clock::now();
 
-        for (int batch = 0; batch < N_SAMPLES; batch += BATCH_SIZE) {
-            for (int d = 0; d < DEPTH; d++) {
-                for (int i = 0; i < N_QUBITS; i++) {
-                    state.act_rx_gate(dist(engine), TARGET);
-                }
-                for (int i = 0; i < N_QUBITS; i++) {
-                    state.act_cnot_gate(TARGET, (TARGET + 1) % N_QUBITS);
+        for (int batch = 0; batch < n_samples; batch += batch_size) {
+            for (int d = 0; d < depth; d++) {
+                if (gate_name == "RX") {
+                    state.act_rx_gate(dist(engine), target);
+                } else if (gate_name == "H") {
+                    state.act_h_gate(target);
+                } else if (gate_name == "T") {
+                    state.act_t_gate(target);
+                } else if (gate_name == "CNOT") {
+                    state.act_cnot_gate(target, control);
                 }
             }
         }
@@ -68,16 +84,16 @@ int main(int argc, char *argv[])
     }
 
     double average = 0.0;
-    for (int trial = 0; trial < N_TRIALS; trial++) {
+    for (int trial = 0; trial < n_trials; trial++) {
         average += durations[trial];
     }
-    average /= N_TRIALS;
+    average /= n_trials;
 
     double variance = 0.0;
-    for (int trial = 0; trial < N_TRIALS; trial++) {
+    for (int trial = 0; trial < n_trials; trial++) {
         variance += (durations[trial] - average) * (durations[trial] - average);
     }
-    variance = variance / N_TRIALS;
+    variance = variance / n_trials;
 
     std::cout << average << std::endl;
 
--- src/random_circuit.cpp
+++ src/random_circuit.cpp
@@ -7,23 +7,24 @@
 
 #include "gate.hpp"
 
-UINT grid_to_id(UINT x, UINT y, UINT length) { return x + length * y; }
+UINT grid_to_id(UINT x, UINT y, UINT width) { return x + width * y; }
 
-bool in_grid(UINT x, UINT y, UINT length) { return x < length && y < length; }
+bool in_grid(UINT x, UINT y, UINT width, UINT height) { return x < width && y < height; }
 
-void act_2q_gate(State &state, UINT LENGTH, UINT x1, UINT y1, UINT x2, UINT y2, double noise_rate)
+void act_2q_gate(State &state, UINT width, UINT height, UINT x1, UINT y1, UINT x2, UINT y2,
+                 double noise_rate)
 {
-    if (!in_grid(x1, y1, LENGTH) || !in_grid(x2, y2, LENGTH)) {
+    if (!in_grid(x1, y1, width, height) || !in_grid(x2, y2, width, height)) {
         return;
     }
 
     static double theta = M_PI / 2;
-    UINT target = grid_to_id(x1, y1, LENGTH);
-    UINT control = grid_to_id(x2, y2, LENGTH);
+    UINT target = grid_to_id(x1, y1, width);
+    UINT control = grid_to_id(x2, y2, width);
 
     state.act_iswaplike_gate(theta, target, control);
 
-    if (noise_rate) {
+    if (noise_rate > 0) {
         state.act_depolarizing_gate_2q(target, control, noise_rate);
     }
 }
@@ -38,15 +39,15 @@ void act_random_1q_gate(State &state, double dice, UINT target, double noise_rat
         state.act_sw_gate(target);
     }
 
-    if (noise_rate > 0.0) {
+    if (noise_rate > 0) {
         state.act_depolarizing_gate_1q(target, noise_rate);
     }
 }
 
-void run_single_batch(State &state, UINT LENGTH, UINT DEPTH, std::mt19937 &engine,
+void run_single_batch(State &state, UINT width, UINT height, UINT DEPTH, std::mt19937 &engine,
                       std::uniform_real_distribution<double> &dist, double noise_rate)
 {
-    UINT N_QUBITS = LENGTH * LENGTH;
+    UINT N_QUBITS = width * height;
 
     for (int d = 0; d < DEPTH; d++) {
         double dice = dist(engine);
@@ -55,20 +56,20 @@ void run_single_batch(State &state, UINT LENGTH, UINT DEPTH, std::mt19937 &engin
             act_random_1q_gate(state, dice, i, noise_rate);
         }
 
-        for (int i = 0; i < LENGTH; i++) {
-            for (int j = 0; j < LENGTH; j++) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 if ((i + j) % 2 != 0) {
                     continue;
                 }
 
                 if (d % 4 == 0) {
-                    act_2q_gate(state, LENGTH, i, j, i + 1, j, noise_rate);
+                    act_2q_gate(state, width, height, i, j, i + 1, j, noise_rate);
                 } else if (d % 4 == 1) {
-                    act_2q_gate(state, LENGTH, i, j, i - 1, j, noise_rate);
+                    act_2q_gate(state, width, height, i, j, i - 1, j, noise_rate);
                 } else if (d % 4 == 2) {
-                    act_2q_gate(state, LENGTH, i, j, i, j + 1, noise_rate);
+                    act_2q_gate(state, width, height, i, j, i, j + 1, noise_rate);
                 } else if (d % 4 == 3) {
-                    act_2q_gate(state, LENGTH, i, j, i, j - 1, noise_rate);
+                    act_2q_gate(state, width, height, i, j, i, j - 1, noise_rate);
                 }
             }
         }
@@ -77,46 +78,54 @@ void run_single_batch(State &state, UINT LENGTH, UINT DEPTH, std::mt19937 &engin
 
 int main(int argc, char *argv[])
 {
-    cxxopts::Options options("qsim-random-circuit", "Noisy random circuit simulator for VE");
+    cxxopts::Options options("qsim-random-circuit", "Noisy random circuit benchmark");
     // clang-format off
     options.add_options()
         ("samples", "# of samples", cxxopts::value<int>()->default_value("1000"))
         ("depth", "Depth of the circuit", cxxopts::value<int>()->default_value("10"))
-        ("length", "Side length of grid", cxxopts::value<int>()->default_value("4"))
+        ("width", "Width of grid", cxxopts::value<int>()->default_value("4"))
+        ("height", "Height of grid", cxxopts::value<int>()->default_value("4"))
         ("batch-size", "Batch size", cxxopts::value<int>()->default_value("1000"))
         ("trials", "# of trials", cxxopts::value<int>()->default_value("10"))
-        ("noise-rate", "Noise rate", cxxopts::value<double>()->default_value("0.1"));
+        ("noise-rate", "Noise rate", cxxopts::value<double>()->default_value("0.1"))
+        ("help", "Print usage");
     // clang-format on
 
     auto result = options.parse(argc, argv);
 
-    const int N_SAMPLES = result["samples"].as<int>();
-    const int DEPTH = result["depth"].as<int>();
-    const int LENGTH = result["length"].as<int>();
-    const int BATCH_SIZE = result["batch-size"].as<int>();
-    const int N_TRIALS = result["trials"].as<int>();
-    const double NOISE_RATE = result["noise-rate"].as<double>();
-    const int N_QUBITS = LENGTH * LENGTH;
+    int n_samples = result["samples"].as<int>();
+    int depth = result["depth"].as<int>();
+    int width = result["width"].as<int>();
+    int height = result["height"].as<int>();
+    int batch_size = result["batch-size"].as<int>();
+    int n_trials = result["trials"].as<int>();
+    double noise_rate = result["noise-rate"].as<double>();
+    int n_qubits = width * height;
+
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
 
     std::random_device seed_gen;
     std::mt19937 engine(seed_gen());
     std::uniform_real_distribution<double> dist(0, 1);
     std::vector<double> durations;
-    State state(N_QUBITS, BATCH_SIZE);
+    State state(n_qubits, batch_size);
 
     // Warmup run
-    for (int batch = 0; batch < N_SAMPLES; batch += BATCH_SIZE) {
+    for (int batch = 0; batch < n_samples; batch += batch_size) {
         state.set_zero_state();
-        run_single_batch(state, LENGTH, DEPTH, engine, dist, NOISE_RATE);
+        run_single_batch(state, width, height, depth, engine, dist, noise_rate);
     }
 
-    for (int trial = 0; trial < N_TRIALS; trial++) {
+    for (int trial = 0; trial < n_trials; trial++) {
         state.set_zero_state();
 
         auto start_time = std::chrono::high_resolution_clock::now();
 
-        for (int batch = 0; batch < N_SAMPLES; batch += BATCH_SIZE) {
-            run_single_batch(state, LENGTH, DEPTH, engine, dist, NOISE_RATE);
+        for (int batch = 0; batch < n_samples; batch += batch_size) {
+            run_single_batch(state, width, height, depth, engine, dist, noise_rate);
         }
 
         auto end_time = std::chrono::high_resolution_clock::now();
@@ -126,10 +135,10 @@ int main(int argc, char *argv[])
     }
 
     double average = 0.0;
-    for (int trial = 0; trial < N_TRIALS; trial++) {
+    for (int trial = 0; trial < n_trials; trial++) {
         average += durations[trial];
     }
-    average /= N_TRIALS;
+    average /= n_trials;
 
     std::cout << average << std::endl;
 
