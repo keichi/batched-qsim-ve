2b6b53238e8800f52d29786893e94598ac2aba9f
Keichi Takahashi <hello@keichi.dev>
2023-09-10 23:18:13 +0900

    Update benchmarking scripts

--- scripts/run_random_circuit.sh
+++ scripts/run_random_circuit.sh
@@ -14,12 +14,12 @@ echo -e "noise_rate\tsamples\tbatch_size\truntime [s]"
 
 samples=100000
 
-for noise_rate in 0.001 0.005 0.01 0.05 0.1
+for noise_rate in 0.001 0.002 0.005 0.01 0.02 0.05 0.1
     do
     for batch_size in 1000 2000 5000 10000 20000 50000
     do
         echo -n -e "$noise_rate\t$samples\t$batch_size\t"
-        ../build/qsim-random-circuit --samples $batch_size --batch-size $batch_size \
+        ../build/qsim-random-circuit --samples $samples --batch-size $batch_size \
                                      --noise-rate $noise_rate --trials 1
     done
 done
