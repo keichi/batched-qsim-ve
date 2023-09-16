9bb7f2dc885d4589d8571d8c7d367ee2d61b392f
Keichi Takahashi <hello@keichi.dev>
2023-09-16 18:44:25 +0900

    Add benchmarking script for CNOT gate

---
+++ scripts/run_cnot_bench.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -q sxs
+#PBS --venode 1
+#PBS -S /bin/bash
+#PBS -l elapstim_req=06:00:00
+
+export OMP_PROC_BIND=true
+
+unset VE_PROGINF
+
+[[ $PBS_O_WORKDIR ]] && cd $PBS_O_WORKDIR
+
+echo -e "qubits\tsamples\tbatch_size\truntime [s]"
+
+samples=100000
+
+for qubits in $(seq 8 14)
+    do
+    for batch_size in 1 10 100 200 500 1000 2000 5000 10000 20000 50000 100000
+    do
+        echo -n -e "$qubits\t$samples\t$batch_size\t"
+        ../build/qsim-gate-bench --qubits $qubits --samples $samples --batch-size $batch_size \
+                                 --gate CNOT --target 1 --control 0
+    done
+done
