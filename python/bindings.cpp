d3e52fdc116fe8fdbb5f0ed45e0b1117ee15f37f
Keichi Takahashi <hello@keichi.dev>
2025-05-23 14:09:32 +0900

    Add function docs

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -7,6 +7,8 @@
 namespace nb = nanobind;
 namespace vq = veqsim;
 
+using namespace nb::literals;
+
 NB_MODULE(_veqsim, m)
 {
     nb::enum_<vq::PauliID>(m, "PauliID")
@@ -16,46 +18,53 @@ NB_MODULE(_veqsim, m)
         .value("Z", vq::PauliID::Z);
 
     nb::class_<vq::PauliOperator>(m, "PauliOperator")
-        .def(nb::init<std::complex<double>, std::vector<UINT>, std::vector<UINT>>());
+        .def(nb::init<std::complex<double>, std::vector<UINT>, std::vector<UINT>>(),
+             "coef"_a, "targets"_a, "pauli_ids"_a);
 
     nb::class_<vq::Observable>(m, "Observable")
         .def(nb::init<>())
-        .def("add_operator", &vq::Observable::add_operator);
+        .def("add_operator", &vq::Observable::add_operator, "operator"_a,
+             "Add a Pauli operator to the observable");
 
     nb::class_<vq::State>(m, "State", "A batched state vector")
-        .def(nb::init<UINT, UINT>(), "Create state vector")
+        .def(nb::init<UINT, UINT>(), "num_qubits"_a, "num_samples"_a, "Create state vector")
         .def_prop_ro("dim", &vq::State::dim, "Dimension")
         .def_prop_ro("batch_size", &vq::State::batch_size, "Batch size")
-        .def("get_vector", &vq::State::get_vector, "Get a single state vector")
-        .def("amplitude", &vq::State::amplitude, "Get the amplitude of a basis")
-        .def("re", &vq::State::re, "Get the real component of the amplitude of a basis")
-        .def("im", &vq::State::im, "Get the imaginary component of the amplitude of a basis")
-        .def("get_probability", &vq::State::get_probability, "Get ")
+        .def("get_vector", &vq::State::get_vector, "sample"_a, "Get a single state vector")
+        .def("amplitude", &vq::State::amplitude, "sample"_a, "i"_a,
+             "Get the amplitude of a basis")
+        .def("re", &vq::State::re, "sample"_a, "i"_a,
+             "Get the real component of the amplitude of a basis")
+        .def("im", &vq::State::im, "sample"_a, "i"_a
+             "Get the imaginary component of the amplitude of a basis")
+        .def("get_probability", &vq::State::get_probability, "i"_a,
+             "Get the probability of a basis to be observed")
         .def("set_zero_state", &vq::State::set_zero_state, "Initialize to |0..0>")
-        .def("act_x_gate", &vq::State::act_x_gate, "Apply an X gate")
-        .def("act_y_gate", &vq::State::act_y_gate, "Apply a Y gate")
-        .def("act_z_gate", &vq::State::act_z_gate, "Apply a Z gate")
-        .def("act_h_gate", &vq::State::act_h_gate, "Apply an H gate")
-        .def("act_t_gate", &vq::State::act_t_gate, "Apply a T gate")
+        .def("act_x_gate", &vq::State::act_x_gate, "target"_a, "Apply an X gate")
+        .def("act_y_gate", &vq::State::act_y_gate, "target"_a, "Apply a Y gate")
+        .def("act_z_gate", &vq::State::act_z_gate, "target"_a, "Apply a Z gate")
+        .def("act_h_gate", &vq::State::act_h_gate, "target"_a, "Apply an H gate")
+        .def("act_t_gate", &vq::State::act_t_gate, "target"_a, "Apply a T gate")
         .def("act_rx_gate", nb::overload_cast<double, UINT>(&vq::State::act_rx_gate),
-             "Act an RX gate with the same angle")
+             "theta"_a, "target"_a, "Act an RX gate with the same angle")
         .def("act_rx_gate",
              nb::overload_cast<const std::vector<double> &, UINT>(&vq::State::act_rx_gate),
-             "Act an RX gate with different angles")
-        .def("act_ry_gate", &vq::State::act_ry_gate, "Apply an RY gate")
-        .def("act_rz_gate", &vq::State::act_rz_gate, "Apply an RZ gate")
-        .def("act_sx_gate", &vq::State::act_sx_gate, "Apply an SX gate")
-        .def("act_sy_gate", &vq::State::act_sy_gate, "Apply an SY gate")
-        .def("act_sw_gate", &vq::State::act_sw_gate, "Apply an SW gate")
-        .def("act_cnot_gate", &vq::State::act_cnot_gate, "Apply a CNOT gate")
-        .def("act_cx_gate", &vq::State::act_cx_gate, "Apply a CX gate")
-        .def("act_cz_gate", &vq::State::act_cz_gate, "Apply a CZ gate")
-        .def("act_iswaplike_gate", &vq::State::act_iswaplike_gate, "Apply an iSWAP-like gate")
+             "theta"_a, "target"_a, "Act an RX gate with different angles")
+        .def("act_ry_gate", &vq::State::act_ry_gate, "theta"_a, "target"_a, "Apply an RY gate")
+        .def("act_rz_gate", &vq::State::act_rz_gate, "theta"_a, "target"_a, "Apply an RZ gate")
+        .def("act_sx_gate", &vq::State::act_sx_gate,  "target"_a, "Apply an SX gate")
+        .def("act_sy_gate", &vq::State::act_sy_gate, "target"_a, "Apply an SY gate")
+        .def("act_sw_gate", &vq::State::act_sw_gate, "target"_a, "Apply an SW gate")
+        .def("act_cnot_gate", &vq::State::act_cnot_gate, "target"_a, "control"_a, "Apply a CNOT gate")
+        .def("act_cx_gate", &vq::State::act_cx_gate, "target"_a, "control"_a, "Apply a CX gate")
+        .def("act_cz_gate", &vq::State::act_cz_gate, "target"_a, "control"_a, "Apply a CZ gate")
+        .def("act_iswaplike_gate", &vq::State::act_iswaplike_gate, "theta"_a, "target"_a,
+              "control"_a, "Apply an iSWAP-like gate")
         .def("act_depolarizing_gate_1q", &vq::State::act_depolarizing_gate_1q,
-             "Apply an one-qubit depolarizing noise gate")
+             "target"_a, "probability"_a, "Apply an one-qubit depolarizing noise gate")
         .def("act_depolarizing_gate_2q", &vq::State::act_depolarizing_gate_2q,
-             "Apply a two-qubit depolarizing noise gate")
-        .def("observe", &vq::State::observe, "Get expectation of a given observable")
+             "target"_a, "control"_a, "probability"_a, "Apply a two-qubit depolarizing noise gate")
+        .def("observe", &vq::State::observe, "observable"_a, "Get expectation of a given observable")
         .def("synchronize", &vq::State::synchronize, "Wait for completion of computation");
 
     auto finalize = [](void *ptr) noexcept { vq::finalize();};
